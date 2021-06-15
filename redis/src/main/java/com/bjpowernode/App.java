e4ff135627f0965473fd16b8c8e8d03a827ae70d
cjc <cjc@163.com>
2021-06-15 17:35:12 +0800

    12

--- redis/src/main/java/com/bjpowernode/App.java
+++ redis/src/main/java/com/bjpowernode/App.java
@@ -7,9 +7,10 @@ public class App
     public static void main( String[] args )
     {
 
-         //创建连接，连接redis服务器
+         //创建连接，连接redis服务器111
         Jedis jedis=new Jedis("127.0.0.1",6379);
         jedis.flushAll();
+        //ewwww
         jedis.set("username","tom");
         System.out.println(jedis.get("username"));
 
